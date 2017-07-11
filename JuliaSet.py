54283b0bef4ba78c68ecb67ef264f3af96eed7d0
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2017-07-11 23:09:54 +0200

    First version

--- JuliaSet.py
+++ JuliaSet.py
@@ -84,14 +84,17 @@ def make_image(data, outputname='res.png', **kwargs):
     plt.close()
 
 
+def getFilename(colormap, c, suffix=''):
+    return colormap + '/res_{0:06d}_{1:06d}{2}.png'.format(int(100000 * c.real), int(100000 * c.imag), suffix)
+
+
 def create_one_julias_set(c=complex(0.0, 0.65), colormap='magma', outputname=None, **kwargs):
-    getFilename = lambda colormap, c, invert: colormap+ '/res_{0:04d}_{1:04d}{2}.png'.format(int(1000 * c.real), int(1000 * c.imag), '_level' if invert else '')
-    if outputname is None:
-        outputname = getFilename(colormap, c, False)
     s = kwargs.get('s', 401)
     x = kwargs.get('x', 2.0)
     invert = kwargs.get('invert', False)
     Z, Z_level = julia_set(w=s, h=s, c=c, re_min=-x, re_max=+x, im_min=-x, im_max=+x, **kwargs)
+    if outputname is None:
+        outputname = getFilename(colormap, c, suffix='')
     make_three_images = False
     if make_three_images:
         ZZ = np.concatenate((np.real(Z), np.abs(Z), np.imag(Z)), axis=1)
@@ -100,18 +103,19 @@ def create_one_julias_set(c=complex(0.0, 0.65), colormap='magma', outputname=Non
         ZZ = np.abs(Z)
         make_image(ZZ, outputname=outputname, colormap=colormap, dpi=s)
     if invert:
-        outputname = getFilename(colormap, c, True)
+        outputname = getFilename(colormap, c, '_level')
         make_image(Z_level, outputname=outputname, colormap=colormap, dpi=s)
+        outputname = getFilename(colormap, c, '_level_mix')
+        make_image(Z_level+np.abs(Z), outputname=outputname, colormap=colormap, dpi=s)
 
 
-def create_several_julias_set(cn, colormap='magma', outputname=None, **kwargs):
+def create_several_julias_set(n, cn, **kwargs):
+    cn = np.linspace(cn[0], cn[1], n)
     for c in cn:
-        create_one_julias_set(c, colormap='magma', outputname=None, **kwargs)
-    raise NotImplementedError
+        create_one_julias_set(c, **kwargs)
 
 
 def create_animated_gif(maxRecursionLevel=6, filename='gosper_curve.gif', **kwargs):
-    tile = kwargs.get('tile', False)
     grid = kwargs.get('grid', False)
     import subprocess
     generateLevel = lambda x: list(range(x)) + [x - i - 2 for i in range(x - 1)]
@@ -119,7 +123,7 @@ def create_animated_gif(maxRecursionLevel=6, filename='gosper_curve.gif', **kwar
     for level in generateLevel(maxRecursionLevel + 1):
         cfilename = filename + '_' + '{0:03d}'.format(level) + '.png'
         cmd += cfilename + ' '
-        plot_level(max_level=level, showAllLevel=False, filename=cfilename, tile=tile, grid=grid)
+        plot_level(max_level=level, showAllLevel=False, filename=cfilename)
     cmd += filename
     subprocess.check_output(cmd.split(' '))
 
@@ -130,11 +134,11 @@ def main():
     pa = parser.add_argument
     pa('-s', '--size', type=int, help='number of recursion level. Reasonnable value is 6', default=401)
     pa('-c', '--colormap', type=str, help='number of recursion level. Reasonnable value is 6', default='viridis')
-    pa('-i', '--invert', action='store_true', help='boolean used to display all levels (disable when tiling)')
+    pa('-i', '--invert', action='store_true', help='boolean used to display all levels')
     pa('-o', '--output', default=None, help='name of the generated file. If not provided, result will display on screen')
-    pa('-x', type=float, default=2.0, help='domain size of the fractal')
+    pa('-x', type=float, default=2.0, help='domain size of the fractal. Default is 2.0, meaning a -2x+2, -2x+2 square will be created')
     pa('-k', type=complex, default=[complex(0.285, 0.01)], nargs='*', help='name of the generated file. If not provided, result will display on screen')
-    pa('-n', '--number', type=int, help='number of pictures to generate between', default=1)
+    pa('-n', '--number', type=int, help='number of pictures to generate between two complex numbers. Default is 2', default=2)
     args = parser.parse_args()
     if args.output is None:
         output = None
@@ -143,7 +147,7 @@ def main():
     if len(args.k)==1:
         create_one_julias_set(c=args.k[0], colormap=args.colormap, outputname=output, s=args.size, x=args.x, invert=args.invert)
     else:
-        create_several_julias_set(c=args.k, colormap=args.colormap, outputname=output, s=args.size, x=args.x, invert=args.invert)
+        create_several_julias_set(n=args.number, cn=args.k, colormap=args.colormap, outputname=output, s=args.size, x=args.x, invert=args.invert)
 
     #if args.output and args.output.lower().endswith('gif'):
     #    create_animated_gif(maxRecursionLevel=args.level, filename=args.output, grid=args.grid, tile=args.tile)
@@ -152,10 +156,4 @@ def main():
 
 
 if __name__ == '__main__':
-    #main()
-    # for i in range(1):
-    #     # create_one_julias_set(c=complex(0.0, 0.01 + i/100.0), colormap='Greys')
-    #     create_one_julias_set(c=complex(0.0, 0.01 + i/100.0), colormap='plasma')
     main()
-    #create_one_julias_set(c=complex(-1.417022285618, 0.0099534), colormap='plasma', outputname='super')
-    #create_one_julias_set(c=complex(0.285, 0.01), colormap='viridis', outputname='super2', s=1001)
